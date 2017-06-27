2156d4bf6554c49fc4ae3833bd6fe9ddab9c337c
Marcio Luis da Silva <marcio.luis.da.silva@accenture.com>
2017-06-27 20:11:28 -0300

    criando rotas do CRUD de usuário

--- meanServer/routes/loginroutes/login.js
+++ meanServer/routes/loginroutes/login.js
@@ -1,7 +1,19 @@
 var express = require('express'); //Importando o modulo do express para trabalhar com rotas
 var rotas = express.Router(); //Obtendo objeto de rotas do express
 
-rotas.get('/teste', function(request, response){
+rotas.get('/user', function(request, response){
+
+
+}).post('/user', function(request, response){
+
+
+}).put('/user', function(request, response){
+
+
+}).delete('/user', function(request, response){
+
+
+}).get('/teste', function(request, response){
     console.log('Rota de teste Login');
     response.end();
 });
