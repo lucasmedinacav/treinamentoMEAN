060fd14cc4b9a0ae668acb1581170f26a23e60ec
ThiagoGuy <thiago.guy@hotmail.com>
2017-06-28 20:31:50 -0300

    Criação da rota de cadastro

--- meanWeb/src/app/app-routing.module.ts
+++ meanWeb/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    redirectTo: '/home', 
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 
---
+++ meanWeb/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Overlay, overlayConfigFactory } from 'angular2-modal';
+import { Modal, BSModalContext } from 'angular2-modal/plugins/bootstrap';
+import { LoginModalContext, LoginModal } from './../login-modal/login-modal.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  title = 'app';
+
+  constructor(public modal: Modal) {}
+
+  ngOnInit() {}
+
+ openLoginModal() {
+   console.log('oeoe');
+   
+    return this.modal.open(LoginModal, overlayConfigFactory({}, BSModalContext));
+  }
+
+}
