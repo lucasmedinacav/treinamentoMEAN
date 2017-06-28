85ade0bc54f4a9a5e0181285233f194e8686ce13
Iara Nascimento Sztybe <iara.sztybe@gmail.com>
2017-06-28 15:13:04 -0300

    adicionando modal inacabado de login

--- meanWeb/src/app/app.component.ts
+++ meanWeb/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Overlay, overlayConfigFactory } from 'angular2-modal';
+import { Modal, BSModalContext } from 'angular2-modal/plugins/bootstrap';
+import { CustomModalContext, CustomModal } from './custom-modal-sample';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [Modal]
 })
 export class AppComponent {
   title = 'app';
+  
+  constructor(public modal: Modal) {}
+
+  openLoginModal() {
+    console.log("abre modal de login");
+    return this.modal.open(CustomModal,  overlayConfigFactory({ num1: 2, num2: 3 }, BSModalContext));
+  }
+
 }
--- meanWeb/src/app/app.module.ts
+++ meanWeb/src/app/app.module.ts
@@ -6,15 +6,23 @@ import { AppComponent } from './app.component';
 
 import "../polyfills";
 
+import { ModalModule } from 'angular2-modal';
+import { BootstrapModalModule } from 'angular2-modal/plugins/bootstrap';
+import { CustomModal } from './custom-modal-sample';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomModal
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModalModule.forRoot(),
+    BootstrapModalModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ CustomModal ]
 })
 export class AppModule { }
