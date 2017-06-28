00a2eefd3624c0f8d67dd36cf1fe76ecdaa8fb2a
Leonardo Santos Andrade <leonrgs@gmail.com>
2017-06-28 20:32:25 -0300

    Recuperando rotas de produto e pedido

--- meanServer/index.js
+++ meanServer/index.js
@@ -4,10 +4,12 @@ var app = express(); //Obtem o 'objeto' express que sera usado para configurar a
 var loginRota = require('./routes/loginroutes/login');
 var produtoRota = require('./routes/produtosroutes/produto');
 var carrinhoComprasRota = require('./routes/carrinhoroutes/carrinho');
+var pedidoRota = require('./routes/pedidosroutes/pedido');
 
 app.use("/login", loginRota);
 app.use("/produto", produtoRota);
 app.use("/carrinho", carrinhoComprasRota);
+app.use("/pedido", pedidoRota)
 
 app.listen(3002, function(){
     console.log('Servidor inicializado');
--- meanServer/routes/pedidosroutes/pedido.js
+++ meanServer/routes/pedidosroutes/pedido.js
@@ -2,23 +2,23 @@ var express = require('express'); //Importando o modulo do express para trabalha
 var rotas = express.Router(); //Obtendo objeto de rotas do express
 
 rotas.get('/buscar', function(request, response){
-    console.log('Rota de busca Produto');
+    console.log('Rota de busca Pedido');
     response.end();
 })
 
 rotas.post('/incluir', function(request, response){
-    console.log('Rota de inclusao Produto');
+    console.log('Rota de inclusao Pedido');
     response.end();
 })
 
 rotas.put('/alterar', function(request, response){
-    console.log('Rota de alteracao Produto');
+    console.log('Rota de alteracao Pedido');
     response.end();
 })
 
-rotas.delete('/excluir', function(request, response){
-    console.log('Rota de exclusao Produto');
-    response.end();
-})
+// rotas.delete('/excluir', function(request, response){
+//     console.log('Rota de exclusao Pedido');
+//     response.end();
+// })
 
 module.exports = rotas;
