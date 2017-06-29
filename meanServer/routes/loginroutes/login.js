49d2f5eb98037e1c8ccc17de0854b488550ca10b
Julio Leme <jleme654@gmail.com>
2017-06-28 22:05:09 -0300

    Versao para testes do primeiro metodo de inclusao de pessoas

--- meanServer/routes/loginroutes/login.js
+++ meanServer/routes/loginroutes/login.js
@@ -1,19 +1,19 @@
 var express = require('express'); //Importando o modulo do express para trabalhar com rotas
 var rotas = express.Router(); //Obtendo objeto de rotas do express
 
-rotas.get('/user', function(request, response){
+rotas.get('/user', function (request, response) {
+    console.log('Rota de teste Login2');
+    response.end();
+}).post('/user', function (request, response) {
 
 
-}).post('/user', function(request, response){
+}).put('/user', function (request, response) {
 
 
-}).put('/user', function(request, response){
+}).delete('/user', function (request, response) {
 
 
-}).delete('/user', function(request, response){
-
-
-}).get('/teste', function(request, response){
+}).get('/teste', function (request, response) {
     console.log('Rota de teste Login2');
     response.end();
 });
