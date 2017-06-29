e43c6daf476389194fd90a93bf52393b6ae5b75e
Valdemir Dinis Vilela Junior <junico.vilela@gmail.com>
2017-06-28 21:09:51 -0300

    carrinho compra

---
+++ meanWeb/src/app/carrinhoCompra/carrinhoCompra.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ListaCarrinhoCompraComponent } from './listaCarrinhoCompra.component';
+import { CarrinhoCompraRoutingModule } from './carrinhoCompra-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    ListaCarrinhoCompraComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    CarrinhoCompraRoutingModule
+    
+  ],
+  exports: [
+    ListaCarrinhoCompraComponent
+  ],
+  providers: [],
+  bootstrap: [ListaCarrinhoCompraComponent]
+})
+export class CarrinhoCompraModule { }
---
+++ meanWeb/src/app/carrinhoCompra/listaCarrinhoCompra.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-lista-carrinho',
+  templateUrl: './listaCarrinhoCompra.component.html',
+  
+})
+export class ListaCarrinhoCompraComponent {
+  
+}
