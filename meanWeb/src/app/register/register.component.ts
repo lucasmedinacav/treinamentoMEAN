431406a05f7ac842ba8ae46fa593921cacbe9312
ThiagoGuy <thiago.guy@hotmail.com>
2017-06-29 22:18:06 -0300

    Adicionado validação no formulário de registro

--- meanWeb/src/app/register/register.component.ts
+++ meanWeb/src/app/register/register.component.ts
@@ -12,19 +12,78 @@ export class RegisterComponent implements OnInit {
 
   public usuario: Usuario;
   public senhaConfirmacao: string;
+  public showMsgError: boolean;
+  public msgError: string;
 
   constructor(public modal: Modal) {
     this.usuario = new Usuario;
-   }
+  }
 
   ngOnInit() { }
 
-  cadastrar(): void {
-    console.log('cadastrado');
+  public cadastrar(): void {
+
+    if (this.usuario.nome == null || this.usuario.sobrenome == null || this.usuario.senha == null || this.usuario.email == null || this.senhaConfirmacao == null) {
+      this.addMsgError();
+    }
+
+    this.showMsgError = this.msgError != null;
+
+    if (!this.showMsgError) {
+      console.log('cadastrado');
+    }
   }
 
-  openLoginModal() {
+  public openLoginModal() {
     return this.modal.open(LoginModal, overlayConfigFactory({}, BSModalContext));
+  }
+
+  public onChangeNome() {
+    let regex = /^(([A-Z]{1})[a-z]+)$/;
+    let test = this.usuario.nome != undefined && regex.test(this.usuario.nome);
+    this.trataValidacao(document.getElementById("nome_form"), test);
+  }
+
+  public onChangeSobrenome() {
+    let regex = /^((([A-Z]{1})([a-z])+(\s)?)+)$/;
+    let test = this.usuario.sobrenome != undefined && regex.test(this.usuario.sobrenome);
+    this.trataValidacao(document.getElementById("sobrenome_form"), test);
+  }
+
+  public onChangeEmail() {
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let test = this.usuario.email != undefined && regex.test(this.usuario.email);
+    this.trataValidacao(document.getElementById("email_form"), test);
+  }
+
+  public onChangeSenha() {
+    console.log("senha: " + this.usuario.senha);
+    let regex = /^.{8,}$/;
+    let test = this.usuario.senha != undefined && regex.test(this.usuario.senha);
+    this.trataValidacao(document.getElementById("senha_form"), test);
+  }
+
+  public onChangeSenhaConfirmacao() {
+    let regex = /^.{8,}$/;
+    let test = this.senhaConfirmacao != undefined && regex.test(this.senhaConfirmacao) && this.usuario.senha == this.senhaConfirmacao;
+    this.trataValidacao(document.getElementById("senhaConfirmacao_form"), test);
+  }
+
+  private trataValidacao(form, test) {
+    this.msgError = null;
+    if (test) {
+      form.classList.remove("has-error");
+      form.classList.add("has-success");
+      this.showMsgError = false;
+    } else {
+      form.classList.remove("has-success");
+      form.classList.add("has-error");
+      this.addMsgError();
+    }
+  }
+
+  private addMsgError() {
+    this.msgError = "Por favor, corrija os campos abaixo corretamente.";
   }
 
 }
