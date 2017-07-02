bba0c80181d781b76ba85d1594c8e5bcf88aedcb
Lucas Medina <lucasmedinacav@hotmail.com>
2017-07-02 16:16:31 -0300

    ajustes

--- meanServer/index.js
+++ meanServer/index.js
@@ -1,10 +1,13 @@
+//CONFIGURANDO MEU PROJETO EXPRESS
 var express = require('express'); //Importando o modulo do express
 var app = express(); //Obtem o 'objeto' express que sera usado para configurar a aplicacao
 
+//BUSCANDO ROTAS QUE VAMOS UTILIZAR COM O EXPRESS
 var loginRota = require('./routes/loginroutes/login');
 var produtoRota = require('./routes/produtosroutes/produto');
 var carrinhoComprasRota = require('./routes/carrinhoroutes/carrinho');
 
+//FALANDO QUE VAMOS UTILIZAR ESSAS ROTAS
 app.use("/login", loginRota);
 app.use("/produto", produtoRota);
 app.use("/carrinho", carrinhoComprasRota);
