522d8df6177fdadde173eaf7efbe951492dbdaef
LEO SANTOS <leo.weverton.santos@gmail.com>
2017-07-03 16:23:12 -0300

    Pagamento por cartao ok

--- meanWeb/src/app/principal/pagamento/pagamento-confirmacao/cartao/cartao.component.ts
+++ meanWeb/src/app/principal/pagamento/pagamento-confirmacao/cartao/cartao.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common'; 
+import { PagamentoService } from '../../pagamento.service';
+import { Router } from '@angular/router'
 
 @Component({
   moduleId: module.id,
@@ -9,4 +11,55 @@ import { Location } from '@angular/common';
 })
 export class CartaoComponent {
  
+  public enderecoEntrega: string = "Avenida: Interlagos, 3501";
+  public cep: string = "02229-020";
+  public cidade: string = "São Paulo - SP";
+
+  public subtotal: number = 166.90;
+  public frete: string;
+  public total: number = 166.90;
+
+  public numeroCartao: string = "";
+  public nomeImpressoCartao: string = "";
+  public validade: string = "";
+  public codigoSeguranca: Number;
+  public quantidadeParcelas: number;
+
+constructor(
+    private location: Location,
+    private pagamentoService: PagamentoService,
+    private rota: Router
+  ) { }
+  voltarParaListaProdutos(): void {
+    this.location.back();
+  }
+
+  pagarComCartaoCredito() {
+    //Boleto bancario
+    // var settings = {
+    //   "Forma": "BoletoBancario"
+    // };
+
+    // Cartao de credito
+    var settings = {
+      Forma: "CartaoCredito",
+      Instituicao: "Visa",
+      Parcelas: "1",
+      Recebimento: "AVista",
+      CartaoCredito: {
+        Numero: this.numeroCartao,
+        Expiracao: this.validade,
+        CodigoSeguranca: this.codigoSeguranca,
+        Portador: {
+          Nome: "Lyoto Machida",
+          DataNascimento: "17/11/1988",
+          Telefone: "(12)9999-9999",
+          Identidade: "888.457.824-84"
+        }
+      }
+    };
+    this.pagamentoService.efeturarPagamentoPorCartaoCredito(settings);
+    this.rota.navigateByUrl("/comprovantePagamento");
+  }
+
 }
--- meanWeb/src/app/principal/pagamento/pagamento-confirmacao/pagamento-confirmacao.component.ts
+++ meanWeb/src/app/principal/pagamento/pagamento-confirmacao/pagamento-confirmacao.component.ts
@@ -11,57 +11,10 @@ declare var moip: any;
 })
 export class PagamentoConfirmacaoComponent {
 
-  public enderecoEntrega: string = "Avenida: Interlagos, 3501";
-  public cep: string = "02229-020";
-  public cidade: string = "São Paulo - SP";
 
-  public subtotal: number = 166.90;
-  public frete: string;
-  public total: number = 166.90;
-
-  public numeroCartao: string = "";
-  public nomeImpressoCartao: string = "";
-  public validade: string = "";
-  public codigoSeguranca: Number;
-  public quantidadeParcelas: number;
-
-  constructor(
-    private location: Location,
-    private pagamentoService: PagamentoService,
-    private rota: Router
-  ) { }
+  
 
 
-  voltarParaListaProdutos(): void {
-    this.location.back();
-  }
-
-  pagarComCartaoCredito() {
-    //Boleto bancario
-    // var settings = {
-    //   "Forma": "BoletoBancario"
-    // };
-
-    // Cartao de credito
-    var settings = {
-      Forma: "CartaoCredito",
-      Instituicao: "Visa",
-      Parcelas: "1",
-      Recebimento: "AVista",
-      CartaoCredito: {
-        Numero: this.numeroCartao,
-        Expiracao: "10/18",
-        CodigoSeguranca: this.codigoSeguranca,
-        Portador: {
-          Nome: "Lyoto Machida",
-          DataNascimento: "17/11/1988",
-          Telefone: "(12)9999-9999",
-          Identidade: "888.457.824-84"
-        }
-      }
-    };
-    this.pagamentoService.efeturarPagamentoPorCartaoCredito(settings);
-    this.rota.navigateByUrl("/comprovantePagamento");
-  }
+  
 
 }
