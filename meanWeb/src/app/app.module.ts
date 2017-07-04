357c3225147d06b66487325377bc635bb0a98838
Lucas Medina <lucasmedinacav@hotmail.com>
2017-07-04 14:43:34 -0300

    ajustes modal

--- meanWeb/src/app/app.module.ts
+++ meanWeb/src/app/app.module.ts
@@ -4,16 +4,17 @@ import { HttpModule } from '@angular/http';
 import { Http } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
-import { ModalModule } from 'angular2-modal';
-import { BootstrapModalModule } from 'angular2-modal/plugins/bootstrap';
 
 import { TranslateService, TranslateLoader, TranslateStaticLoader } from 'ng2-translate';
 
+import { BootstrapModalModule, Modal } from 'angular2-modal/plugins/bootstrap';
+import { ModalModule, OverlayRenderer, DOMOverlayRenderer, Overlay } from 'angular2-modal';
 import { AppRoutingModule } from './app-routing.module';
 import { PrincipalModule } from './principal/principal.module';
 import { TranslateModule } from "ng2-translate";
 
 import { LoginModal } from './login-modal/login-modal.component';
+
 import { RegisterComponent } from './register/register.component';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -39,7 +40,7 @@ import "../polyfills";
     PrincipalModule,
     BootstrapModalModule,
     FormsModule],
-  providers: [],
+  providers: [Modal, Overlay, { provide: OverlayRenderer, useClass: DOMOverlayRenderer }],
   bootstrap: [AppComponent, NavbarComponent],
   entryComponents: [LoginModal]
 })
